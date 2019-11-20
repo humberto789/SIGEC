d10763e48cffd8da9c67a07bb1fabb50355e4afe
brayanleo1 <bleogalaxy@gmail.com>
2019-11-20 19:47:24 -0300

    Update ChamarPaciente.java

--- src/br/com/SIGEC/web/mbean/ChamarPaciente.java
+++ src/br/com/SIGEC/web/mbean/ChamarPaciente.java
@@ -2,10 +2,17 @@ package br.com.SIGEC.web.mbean;
 
 import br.com.SIGEC.model.Sala;
 
+/*Lembrete: cada lista terá um nome esapecífico com pacientes específicos que participaram dessa lista dependendo de suas necessidades,
+ * Salas que tratam do mesmo problema podem ter uma lista compartilhada? Os pacientes terão responsáveis(médicos) específicos?*/
+
+//O que devo fazer:
 //Médico disponibiliza sala --?--
-//listaDeEspera da sala tem status atualizado --C--
-//verifica próximo paciente da lista --C--
-//se existe paciente, chama ele e atualiza a lista e estado da sala --?C--
+//ListaDeEspera da sala tem status atualizado --C--
+//Verifica próximo paciente da lista --C--
+//Se existe paciente, chama ele e atualiza a lista e estado da sala --?C-- (Por onde chamo ele? Onde fica aquela tela?)
+//Aviso ao médico e mando os dados do próximo paciente --?-- (Para onde mando? Só mando o nome? Cpf também ou algo a mais?)
+//Se não existe paciente, aviso ao médico --C-- (Para onde mando?)
+
 
 public class ChamarPaciente {
 	//botão próximo acionado
