e3ee4507da7104177a59821f840d940293387604
Georgevaldo <57221417+Georgevaldo@users.noreply.github.com>
2019-11-20 18:04:52 -0300

    Update ConfirmarConsulta.java

--- src/br/com/SIGEC/model/ConfirmarConsulta.java
+++ src/br/com/SIGEC/model/ConfirmarConsulta.java
@@ -2,8 +2,8 @@ package br.com.SIGEC.model;
 
 public class ConfirmarConsulta {
 
-	//import org.apache.commons.mail.DefaultAuthenticator;
-	//import org.apache.commons.mail.SimpleEmail;
+	// import org.apache.commons.mail.DefaultAuthenticator;
+	// import org.apache.commons.mail.SimpleEmail;
 
 	private String remetente;
 	private String destinatario;
@@ -17,60 +17,60 @@ public class ConfirmarConsulta {
 		this.assunto = assunto;
 		this.msg = msg;
 	}
+
 	private String getRemetente() {
 		return remetente;
 	}
+
 	private void setRemetente(String remetente) {
 		this.remetente = remetente;
 	}
+
 	private String getDestinatario() {
 		return destinatario;
 	}
+
 	private void setDestinatario(String destinatario) {
 		this.destinatario = destinatario;
 	}
+
 	private String getAssunto() {
 		return assunto;
 	}
+
 	private void setAssunto(String assunto) {
 		this.assunto = assunto;
 	}
+
 	private String getMsg() {
 		return msg;
 	}
+
 	private void setMsg(String msg) {
 		this.msg = msg;
 	}
 
 	public void enviarEmail() {
-		
-	}
-
-		//Enviar email
-		
-		/*	public static void main(String[] args) { //Usar static tá certo?
-			String meuemail = "sigec.info4m@gmail.com";
-			String minhasenha = "sigec2019";
-			String destinatario = "george.costa25.gc@gmail.com";
-			String msg = "teste";
-
-			SimpleEmail email = new SimpleEmail();
-			email.setHostName("smtp.gmail.com");
-			email.setSmtpPort(465);
-			email.setAuthenticator(new DefaultAuthenticator(meuemail, minhasenha));
-			email.setSSLOnConnect(true);
-
-			try {
-				email.setFrom(meuemail);
-				email.setSubject("SIGEC - CONFIRMAÇÃO DE CONSULTA");
-				email.setMsg(msg);
-				email.addTo(destinatario);
-				email.send();
-				System.out.println("email enviado!");
-
-			} catch (Exception e) {
-				e.printStackTrace();
-			}
-		} */
 
 	}
+
+	// Enviar email
+
+	/*
+	 * public static void main(String[] args) { //Usar static tá certo? String
+	 * meuemail = "sigec.info4m@gmail.com"; String minhasenha = "sigec2019"; String
+	 * destinatario = "george.costa25.gc@gmail.com"; String msg = "teste";
+	 * 
+	 * SimpleEmail email = new SimpleEmail(); email.setHostName("smtp.gmail.com");
+	 * email.setSmtpPort(465); email.setAuthenticator(new
+	 * DefaultAuthenticator(meuemail, minhasenha)); email.setSSLOnConnect(true);
+	 * 
+	 * try { email.setFrom(meuemail);
+	 * email.setSubject("SIGEC - CONFIRMAÇÃO DE CONSULTA"); email.setMsg(msg);
+	 * email.addTo(destinatario); email.send();
+	 * System.out.println("email enviado!");
+	 * 
+	 * } catch (Exception e) { e.printStackTrace(); } }
+	 */
+
+}
