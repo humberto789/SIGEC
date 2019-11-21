f8abc1743d1a7d1b3fc8cb0e69d0e1c2ecc50e35
juliusneto <57206215+juliusneto@users.noreply.github.com>
2019-11-21 16:30:22 -0300

    Dao's

--- src/br/com/SIGEC/test/chamarPacienteTest.java
+++ src/br/com/SIGEC/test/chamarPacienteTest.java
@@ -4,27 +4,22 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.Assert;
 
+import br.com.SIGEC.control.ChamarPacienteDAO;
+import br.com.SIGEC.control.GuicheDAO;
 import br.com.SIGEC.model.Fila;
-import br.com.SIGEC.web.mbean.ChamarPaciente;
-import br.com.SIGEC.web.mbean.GuicheMB;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 class chamarPacienteTest {
 	
-	void adiciona(){
-	GuicheMB.conexaoComBancoMySQL();
-	System.out.println();
-	}
-	
 	@Test
 	void testComPaciente() {
 		//Cenário
 		Fila fila = new Fila();
-		ChamarPaciente proximo = new ChamarPaciente();
-		GuicheMB.conexaoComBancoMySQL();
-		proximo.RecuperaLista(fila);
+		ChamarPacienteDAO proximo = new ChamarPacienteDAO();
+		GuicheDAO.guicheSenha();
+		proximo.recuperarLista(fila);
 		
 		//Teste de cenário
 		Assertions.assertEquals("1", proximo.chamarProximo(fila));
