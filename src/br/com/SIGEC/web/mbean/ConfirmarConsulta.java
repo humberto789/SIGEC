8e36253d3bd1bd42f584fc3c554e0d6bee089713
Georgevaldo <57221417+Georgevaldo@users.noreply.github.com>
2019-11-21 15:54:28 -0300

    Update ConfirmarConsulta.java

--- src/br/com/SIGEC/web/mbean/ConfirmarConsulta.java
+++ src/br/com/SIGEC/web/mbean/ConfirmarConsulta.java
@@ -2,50 +2,39 @@ package br.com.SIGEC.web.mbean;
 
 import org.apache.commons.mail.DefaultAuthenticator;
 import org.apache.commons.mail.SimpleEmail;
-import br.com.SIGEC.model.EmailConsulta;
-import br.com.SIGEC.model.Usuario;
+
+//import org.apache.commons.mail.DefaultAuthenticator;
+//import org.apache.commons.mail.SimpleEmail;
+//import br.com.SIGEC.model.EmailConsulta;
+//import br.com.SIGEC.model.Usuario;
 
 public class ConfirmarConsulta {
 
-	// Dúvidas:
-	// ----> Como a mensagem vai funcionar?
-	// ----> O que enviarEmail vai retornar?
+	public void enviarEmail() {
+		String meuemail = "sigec.info4m@gmail.com";
+		String minhasenha = "sigec2019";
+		String destinatario = "george.costa25.gc@gmail.com";
+		String msg = "teste";
 
+		SimpleEmail email = new SimpleEmail();
+		email.setHostName("smtp.gmail.com");
+		email.setSmtpPort(465);
+		email.setAuthenticator(new DefaultAuthenticator(meuemail, minhasenha));
+		email.setSSLOnConnect(true);
 
-	// Enviar Email
-	private String enviarEmail (EmailConsulta email, Usuario user) {
-
-		// Coletando informações
-		String meuemail = email.getRemetente();
-		String minhasenha = email.getSenha();
-		String destinatario = user.getEmail();
-		String msg = email.getMensagem();
-		String subject = email.getAssunto();
-
-		// Acessar o GMAIL
-		SimpleEmail emailConfig = new SimpleEmail();
-		emailConfig.setHostName("smtp.gmail.com");
-		emailConfig.setSmtpPort(465);
-		emailConfig.setAuthenticator(new DefaultAuthenticator(meuemail, minhasenha));
-		emailConfig.setSSLOnConnect(true);
-
-		// Informar os dados do email
-		
-		// if (marcar = true) { // Criar a classe MarcarConsulta
 		try {
-			emailConfig.setFrom(meuemail);
-			emailConfig.setSubject(subject);
-			emailConfig.setMsg(msg);
-			emailConfig.addTo(destinatario);
-			emailConfig.send();
+			email.setFrom(meuemail);
+			email.setSubject("SIGEC - CONFIRMAÇÃO DE CONSULTA");
+			email.setMsg(msg);
+			email.addTo(destinatario);
+			email.send();
+			System.out.println("email enviado!");
+
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		// }
-
-		return msg; // Precisa retornar o que? Retornar para ConfirmarPresença
-
 	}
+
 }
 
 // Acessar o banco
