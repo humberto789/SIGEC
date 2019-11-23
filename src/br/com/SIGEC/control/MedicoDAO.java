6f178578e2ffdc54a5717ca0860e394a284ec260
Pierre Brito <carlospierre07@gmail.com>
2019-11-23 01:46:46 -0300

    Implementação da agenda com o BD

--- src/br/com/SIGEC/control/MedicoDAO.java
+++ src/br/com/SIGEC/control/MedicoDAO.java
@@ -1,13 +1,21 @@
 package br.com.SIGEC.control;
 
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
-
+import java.util.ArrayList;
+import java.util.List;
 import br.com.SIGEC.model.Medico;
+import br.com.SIGEC.model.Paciente;
+import br.com.SIGEC.model.Pessoa;
+import br.com.SIGEC.web.mbean.Consulta;
 
 public class MedicoDAO extends AbstractDao {
 
 	private static final String SQL_INSERT_MEDICO = "insert into medico (crm, id_pessoa) values (?, (select id from pessoa where cpf = ?)); ";
+	private static final String SQL_SELECT_MEDICO_CONSULTAS = "SELECT cons.dataConsulta, pes.nome, pes.cpf FROM consulta cons INNER JOIN paciente p ON cons.id_paciente = p.id " + 
+																															  "INNER JOIN pessoa pes ON p.id_pessoa = pes.id " + 
+																															  "WHERE id_medico = ?;";
 
 	public static boolean cadastroMedico(Medico medico) {
 
@@ -40,6 +48,43 @@ public class MedicoDAO extends AbstractDao {
 		}
 
 		return false;
+	}
+	
+	/**
+	 * De onde vai vir esse médico? Ele vai estar logado? Vai colocar o CPF dele para pesquisar as consultas?
+	 * @param medico - Médico das consultas
+	 * @return - Lista de consultas desse médico
+	 */
+	
+	public static List<Consulta> listarConsultas(Medico medico) {
+		List<Consulta> consultas = new ArrayList<>();
+		
+		try {
+			PreparedStatement stmt = AbstractDao.getConexao().prepareStatement(SQL_SELECT_MEDICO_CONSULTAS);
+			stmt.setInt(1, medico.getId());
+			ResultSet rs = stmt.executeQuery();
+			
+			while (rs.next()) {
+				
+				Paciente paciente = new Paciente();
+				Pessoa pessoa = new Pessoa();
+				pessoa.setNomeCompleto(rs.getString("pes.nome"));
+				pessoa.setCpf(rs.getString("pes.cpf"));
+				paciente.setPessoa(pessoa);
+				
+				Consulta consulta = new Consulta();
+				consulta.setMedico(medico.getPessoa().getNomeCompleto());	
+				consulta.setDataConsulta(rs.getDate("cons.dataConsulta"));
+				consulta.setPaciente(pessoa.getNomeCompleto());
+
+				consultas.add(consulta);
+			}
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	
+		return consultas;
 	}
 
 }
--- src/br/com/SIGEC/web/mbean/AgendaMedica.java
+++ src/br/com/SIGEC/web/mbean/AgendaMedica.java
@@ -2,15 +2,19 @@ package br.com.SIGEC.web.mbean;
 
 import java.time.LocalDateTime;
 import java.time.ZoneId;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 import javax.annotation.PostConstruct;
 import javax.faces.bean.ManagedBean;
-import javax.faces.bean.ViewScoped;
-
 import org.primefaces.model.DefaultScheduleEvent;
 import org.primefaces.model.DefaultScheduleModel;
 import org.primefaces.model.ScheduleModel;
+
+import br.com.SIGEC.control.MedicoDAO;
+import br.com.SIGEC.model.Medico;
+import br.com.SIGEC.model.Pessoa;
 
 /**
  * 
@@ -19,7 +23,6 @@ import org.primefaces.model.ScheduleModel;
  *Essa classe é só para iniciar o componente de agenda do Primefaces
  */
 @ManagedBean
-@ViewScoped
 public class AgendaMedica {
 	private ScheduleModel eventModel;
 	
@@ -30,12 +33,41 @@ public class AgendaMedica {
 		DefaultScheduleEvent consulta = new DefaultScheduleEvent("Exame do toque", hoje1DaTarde(), hoje2DaTarde());
 		consulta.setDescription("Vá com calma com paciente, pois já está com 80 anos.");
 		this.eventModel.addEvent(consulta);
+		
+		Medico medico = new Medico();
+		medico.setId(1);
+		Pessoa pessoa = new Pessoa();
+		pessoa.setNomeCompleto("Pierre Carlos de Brito");
+		medico.setPessoa(pessoa);
+		
+		carregarConsultasNaAgenda(medico);
+		
 	}
+	
+	private void  carregarConsultasNaAgenda(Medico medico) {
+		List<DefaultScheduleEvent> eventosConsultas = new ArrayList<>();
+		
+		List<Consulta> consultas = MedicoDAO.listarConsultas(medico);
+		consultas.forEach(c -> {
+			DefaultScheduleEvent evento = new DefaultScheduleEvent("Consulta com " + c.getPaciente(), c.getDataConsulta(), ultimaHoraDeHoje());
+			evento.setDescription("Consulta marcada com o paciente de CPF...");
+			evento.setAllDay(true);
+			eventosConsultas.add(evento);
+		});
+		
+		eventosConsultas.forEach(e -> this.eventModel.addEvent(e));
+	}
+	
+	private Date ultimaHoraDeHoje() {
+        LocalDateTime ldt = LocalDateTime.now().withHour(23).withMinute(59).withSecond(59).withNano(0);
+        return Date.from(ldt.atZone(ZoneId.systemDefault()).toInstant());
+    }
 	
 	private Date hoje1DaTarde() {
         LocalDateTime ldt = LocalDateTime.now().withHour(13).withMinute(0).withSecond(0).withNano(0);
         return Date.from(ldt.atZone(ZoneId.systemDefault()).toInstant());
     }
+	
 	
 	private Date hoje2DaTarde() {
         LocalDateTime ldt = LocalDateTime.now().withHour(14).withMinute(0).withSecond(0).withNano(0);
