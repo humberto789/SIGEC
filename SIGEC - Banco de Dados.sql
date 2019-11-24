a18b8efb87387da6efa22ff910253b932c542001
humberto789 <56692368+humberto789@users.noreply.github.com>
2019-11-24 01:31:27 -0300

    Mudança nos atributos login e senha do Usuario; Novos inserts para testes
    login e senha deixaram de ser unicos

--- SIGEC - Banco de Dados.sql
+++ SIGEC - Banco de Dados.sql
@@ -32,8 +32,8 @@ create table telefone(
     foreign key(id_pessoa) references pessoa(id) on delete restrict on update cascade
 );
 create table usuario(
-	login varchar(100) unique not null,
-    senha varchar(50) unique not null,
+	login varchar(100) not null,
+    senha varchar(50) not null,
     email varchar(100),
     ativo boolean not null,
     id int auto_increment not null,
@@ -49,6 +49,18 @@ create table medico(
     foreign key(id_pessoa) references pessoa(id) on delete restrict on update cascade
 );
 create table paciente(
+	id int auto_increment not null,
+	id_pessoa int not null,
+	primary key(id),
+    foreign key(id_pessoa) references pessoa(id) on delete restrict on update cascade
+);
+create table recepcionista(
+	id int auto_increment not null,
+	id_pessoa int not null,
+	primary key(id),
+    foreign key(id_pessoa) references pessoa(id) on delete restrict on update cascade
+);
+create table administrador(
 	id int auto_increment not null,
 	id_pessoa int not null,
 	primary key(id),
@@ -100,15 +112,37 @@ INSERT INTO pessoa(cpf, nome, dataNascimento, sexo) value("705.960.664-31", "Mal
 INSERT INTO pessoa(cpf, nome, dataNascimento, sexo) value("705.960.664-32", "José", "1990-11-11", "masculino");
 INSERT INTO pessoa(cpf, nome, dataNascimento, sexo) value("705.960.664-33", "Maria", "1991-11-11", "feminino");
 INSERT INTO pessoa(cpf, nome, dataNascimento, sexo) value("705.960.664-34", "George", "1999-11-11", "masculino");
+INSERT INTO pessoa(cpf, nome, dataNascimento, sexo) value("111.111.111-11", "Humberto", "2000-11-11", "masculino");
+INSERT INTO pessoa(cpf, nome, dataNascimento, sexo) value("222.222.222-22", "Hellen", "1998-11-11", "feminino");
+INSERT INTO pessoa(cpf, nome, dataNascimento, sexo) value("333.333.333-33", "Isabel", "1997-11-11", "feminino");
+INSERT INTO pessoa(cpf, nome, dataNascimento, sexo) value("444.444.444-44", "Julio", "1996-11-11", "masculino");
+INSERT INTO pessoa(cpf, nome, dataNascimento, sexo) value("555.555.555-55", "Pierre", "1995-11-11", "masculino");
 
 INSERT INTO usuario(login, senha, email, ativo, id_pessoa) values("705.960.664-31", "odeiohomens", "malévolamalvadinha@hotmail.com", true, (select id from pessoa where cpf = "705.960.664-31"));
+INSERT INTO usuario(login, senha, email, ativo, id_pessoa) values("705.960.664-33", "12345", "maria@gmail.com", true, (select id from pessoa where cpf = "705.960.664-33"));
+INSERT INTO usuario(login, senha, email, ativo, id_pessoa) values("123456", "12345", "jose@gmail.com", true, (select id from pessoa where cpf = "705.960.664-32"));
 INSERT INTO usuario(login, senha, email, ativo, id_pessoa) values("705.960.664-34", "odeiohomens2", "george.costa25.gc@gmail.com", true, (select id from pessoa where cpf = "705.960.664-34"));
+INSERT INTO usuario(login, senha, email, ativo, id_pessoa) values("111111", "12345", "humberto@gmail.com", true, (select id from pessoa where cpf = "111.111.111-11"));
+INSERT INTO usuario(login, senha, email, ativo, id_pessoa) values("222.222.222-22", "12345", "hellen@gmail.com", true, (select id from pessoa where cpf = "222.222.222-22"));
+INSERT INTO usuario(login, senha, email, ativo, id_pessoa) values("333.333.333-33", "12345", "isabel@gmail.com", true, (select id from pessoa where cpf = "333.333.333-33"));
+INSERT INTO usuario(login, senha, email, ativo, id_pessoa) values("444.444.444-44", "12345", "julio@gmail.com", true, (select id from pessoa where cpf = "444.444.444-44"));
+INSERT INTO usuario(login, senha, email, ativo, id_pessoa) values("555.555.555-55", "12345", "pierre@gmail.com", true, (select id from pessoa where cpf = "555.555.555-55"));
 
 INSERT INTO paciente(id_pessoa) value((select id from pessoa where cpf = "705.960.664-31"));
 INSERT INTO paciente(id_pessoa) value((select id from pessoa where cpf = "705.960.664-33"));
+
 INSERT INTO medico(crm, id_pessoa) value("123456", (select id from pessoa where cpf = "705.960.664-32"));
+INSERT INTO medico(crm, id_pessoa) value("111111", (select id from pessoa where cpf = "111.111.111-11"));
+
+INSERT INTO recepcionista(id_pessoa) value((select id from pessoa where cpf = "222.222.222-22"));
+INSERT INTO recepcionista(id_pessoa) value((select id from pessoa where cpf = "333.333.333-33"));
+
+INSERT INTO administrador(id_pessoa) value((select id from pessoa where cpf = "444.444.444-44"));
+INSERT INTO administrador(id_pessoa) value((select id from pessoa where cpf = "555.555.555-55"));
 
 INSERT INTO atestado(cid, dataEmissao, dataVencimento, id_medico, id_paciente) VALUES("f41", "2019-10-10", "2019-10-12", (select medico.id from medico inner join pessoa on medico.id_pessoa = pessoa.id where cpf = "705.960.664-32"), (select paciente.id from paciente inner join pessoa on paciente.id_pessoa = pessoa.id where cpf = "705.960.664-31"));
 INSERT INTO atestado(cid, dataEmissao, dataVencimento, id_medico, id_paciente) VALUES("f45", "2019-10-13", "2019-10-16", (select medico.id from medico inner join pessoa on medico.id_pessoa = pessoa.id where cpf = "705.960.664-32"), (select paciente.id from paciente inner join pessoa on paciente.id_pessoa = pessoa.id where cpf = "705.960.664-33"));
 
 
+
+SELECT * FROM paciente INNER JOIN pessoa ON paciente.id_pessoa = pessoa.id INNER JOIN usuario ON usuario.id_pessoa = pessoa.id WHERE usuario.login = "705.960.664-31";
