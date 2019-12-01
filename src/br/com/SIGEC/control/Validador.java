f148f5e2fedb8eb96be40c6251036e01e8856929
humberto789 <56692368+humberto789@users.noreply.github.com>
2019-11-30 23:36:58 -0300

    Aperfeiçoamento do cadastro do médico
    A validação de email e senha foram adicionados.
    O cadastro do telefone e do endereço já está funcionando junto com o cadastro do medico.
    As máscaras foram adicionadas no xhtml.

--- src/br/com/SIGEC/control/Validador.java
+++ src/br/com/SIGEC/control/Validador.java
@@ -1,15 +1,20 @@
 package br.com.SIGEC.control;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Validador {
 	
-	public static boolean validadorEmail(String email) {
+public static boolean validadorEmail(String email) {
 		
 		if(email != null && email.length() > 0) {
-			
-			String expressaoEmail = "^[\\\\w\\\\.-]+@([\\\\w\\\\-]+\\\\.)+[A-Z]{2,4}$";
-			
-			return email.matches(expressaoEmail);
-			
+			String expressaoEmail = "^([a-zA-Z0-9_\\-\\.]+)@((\\[[0-9]{1,3}"
+					+ "\\.[0-9]{1,3}\\.[0-9]{1,3}\\.)|(([a-zA-Z0-9\\-]+\\"
+					+ ".)+))([a-zA-Z]{2,4}|[0,9]{1,3})(\\]?)$";
+
+			Pattern pattern = Pattern.compile(expressaoEmail);
+			Matcher matcher = pattern.matcher(email);
+			return matcher.find() && matcher.group().equals(email);
 		}
 		return false;
 	}
@@ -30,7 +35,7 @@ public class Validador {
 	
 	//A senha tem que ter pelo menos três desses caracteres(letra maiuscula, letra minuscula, numero ou simbolo), deve ser maior que 6 caracteres e não deve conter espaço
 	public static boolean validadorSenha(String senha) {
-	    if (senha.length() < 6) {
+	    if (senha.length() >= 6) {
 	    	
 	    	boolean achouNumero = false;
 	        boolean achouMaiuscula = false;
--- src/br/com/SIGEC/web/mbean/CadastrarMedicoMB.java
+++ src/br/com/SIGEC/web/mbean/CadastrarMedicoMB.java
@@ -4,6 +4,8 @@ import javax.faces.bean.ManagedBean;
 import javax.faces.bean.RequestScoped;
 
 import br.com.SIGEC.control.MedicoDAO;
+import br.com.SIGEC.control.UsuarioDAO;
+import br.com.SIGEC.control.Validador;
 import br.com.SIGEC.model.Medico;
 
 @ManagedBean
@@ -17,10 +19,26 @@ public class CadastrarMedicoMB extends AbstractMBean {
 	}
 
 	public void cadastrar() {
-		if (MedicoDAO.cadastroMedico(medico)) {
-			super.exibirMensagemInformativa("Cadastro feito com sucesso");
+		
+		if(Validador.validadorSenha(medico.getPessoa().getUsuario().getSenha())) {
+			if(Validador.validadorEmail(medico.getPessoa().getUsuario().getEmail())) {
+				if(UsuarioDAO.buscarUsuarioPorLoginESenha(medico.getPessoa().getUsuario().getLogin(), medico.getPessoa().getUsuario().getSenha())==null) {
+					if (MedicoDAO.cadastroMedico(medico)) {
+						super.exibirMensagemInformativa("Cadastro feito com sucesso");
+					}else {
+						super.exibirMensagemDeErro("Houve algum problema no cadastro");
+					}
+				}else {
+					super.exibirMensagemDeErro("Login e/ou senha já existem");
+				}
+			}else {
+				super.exibirMensagemDeErro("Email inválido");
+			}
 		}
-		super.exibirMensagemDeErro("Deu algum erro, se vira");
+		else {
+			super.exibirMensagemDeErro("Senha inválida: Tem que ter no minimo tres tipos de caracteres");
+		}
+		
 	}
 
 	public Medico getMedico() {
